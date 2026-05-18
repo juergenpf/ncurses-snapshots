3c930d59306ad220222579c0ce331909f3a840f3
Juergen Pfeifer <juergen@familiepfeifer.de>
2026-05-18 14:42:23 +0200

    Patches from Thomas Dickey, mosty formatting issues

--- ncurses/tinfo/lib_termselect.c
+++ ncurses/tinfo/lib_termselect.c
@@ -1,5 +1,5 @@
 /****************************************************************************
- * Copyright 2026 Juergen Pfeifer                                *
+ * Copyright 2026 Juergen Pfeifer                                           *
  *                                                                          *
  * Permission is hereby granted, free of charge, to any person obtaining a  *
  * copy of this software and associated documentation files (the            *
@@ -35,33 +35,35 @@
 MODULE_ID("$Id$")
 
 /*
- * This routine is a repacement for a simple getenv("TERM"). It is used for environments
- * like Windows, where we do not have terminfo and it's unusual to have an environment
- * variable TERM.
+ * This routine is a replacement for a simple getenv("TERM").  It is used for
+ * environments like Windows, where we do not have terminfo and it's unusual to
+ * have an environment variable TERM.
  * 
- * The idea is, that this routine initializes potential platform specific subsystems like
- * for example the Console subsystem on Windows and then returns a name that can be 
- * interpreted as the result of getenv("TERM"). 
+ * The idea is, that this routine initializes potential platform specific
+ * subsystems like for example the Console subsystem on Windows and then
+ * returns a name that can be interpreted as the result of getenv("TERM"). 
  * 
- * IUf the resulting name starts with a "#", which cannot be a valid terminfo name, we know
- * we are in a non-terminfo environment. Any other result, including NULL or an empty string,
- * will result in a terminfo processing, e.g. lookup in the database or whatever is configured.
+ * If the resulting name starts with a "#", which cannot be a valid terminfo
+ * name, we know we are in a non-terminfo environment.  Any other result,
+ * including NULL or an empty string, will result in a terminfo processing,
+ * e.g. lookup in the database or whatever is configured.
 */
 NCURSES_EXPORT(char *)
-_nc_term_select (void)
+_nc_term_select(void)
 {
 #if USE_CONSOLE_API
     if (!CoreConsoleInitialized()) {
-	    if (!_nc_console_setup()) {
-	        fprintf(stderr, CONSOLE_INIT_FAILURE_MSG);
-            /* This is part of termlib and detected early in the initialization phase,
-            *  so - for now - we skip formal release  of potentially allocated resources
-            *  if there are any at all in that early stage. */
-	        exit(EXIT_FAILURE);
-	    }
+	if (!_nc_console_setup()) {
+	    fprintf(stderr, CONSOLE_INIT_FAILURE_MSG);
+	    /* This is part of termlib and detected early in the initialization
+	     * phase, so - for now - we skip formal release of potentially
+	     * allocated resources if there are any at all in that early stage. 
+	     */
+	    exit(EXIT_FAILURE);
+	}
     }
     assert(DefaultConsole() != NULL);
     return (DefaultConsole()->termname());
 #endif
-    return(getenv("TERM"));
+    return (getenv("TERM"));
 }
